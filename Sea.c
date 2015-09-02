3e6c7061fd67f5497a2b7b9dbab2e5bf95885e66
marvelmugen@gmail.com <marvelmugen@gmail.com>
2015-09-02 14:20:41 -0400

    Branch Ccode created

---
+++ Sea.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+
+FILE *mercuryFile;
+mercuryFile=fopen("mercurydata.csv","r");
+char info[10000]; //When this was an ara it printed correc thins sort of
+
+while (fscanf(mercuryFile,"%s", info)!=EOF){
+
+printf("%s",info);               
+}
+}     
