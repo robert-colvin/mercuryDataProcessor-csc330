f4277263271a68def3bc9bc27ef6e6796fd6c69e
marvelmugen@gmail.com <marvelmugen@gmail.com>
2015-09-04 14:41:35 -0400

    The code of the sea be a foul mistress.

--- Sea.c
+++ Sea.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
 
-
-int main(){
+int main(){ 
 
 FILE *mercuryFile;
 mercuryFile=fopen("mercurydata.csv","r");
-char info[10000]; //When this was an ara it printed correc thins sort of
-
+char info[100000]; 
+char *pointer;
+char *end;
 while (fscanf(mercuryFile,"%s", info)!=EOF){
-
-printf("%s",info);               
+pointer=strtok(info,", \n");
+while(pointer!=NULL){
+double Data=atof(pointer);    
+printf("%.10f\n",Data); 
+pointer=strtok(NULL,", \n");        
+}
+printf("\n",NULL);
 }
 }     
