0367edee5dea81048c9a2d58d3be7a6265af1801
Jonathan Oakes <darth62969@gmail.com>
2015-09-16 13:03:31 -0400

    updates stuffings

--- this.f90
+++ this.f90
@@ -6,16 +6,17 @@
         DOUBLE PRECISION, DIMENSION (3260) ::  mom
         DOUBLE PRECISION, DIMENSION (3260) ::  dad
 
-        open ( unit=8, file='./mercurydata.csv',
-     C      status='old')
+        INTEGER cnt
 
-        real cnt=0
-10      read (8,*,END=20) lat(cnt),long(cnt),merc (cnt)
+        open ( unit=8, file='./mercurydata.csv', status='old')
+
+
+10      read (8,*,END=20) lat(cnt), long(cnt), merc (cnt)
         
-        print *, 'latitude: ', lat (cnt) 
-        print *, 'longitude: ', long (cnt) 
-        print *, 'mercury: ', merc (cnt)
-        print *
+ !       print *, 'latitude: ', lat (cnt) 
+  !      print *, 'longitude: ', long (cnt) 
+   !     print *, 'mercury: ', merc (cnt)
+    !    print *
        
         cnt = cnt + 1
 
@@ -27,13 +28,10 @@
 
         
         do i=0,3260,1
-                do j=0,3260,1
-                        mom(j) = (SIN((lat(i)-lat(j))/2.0))**2 
-                        dad(j) = COS(lat(i)) * COS(lat(j)) 
-                        dad(j) = dad(j) * (SIN((long(i)-long(j))/2.0))**2
-                        mom(j) = mom(j) + dad(j)
-                        dad(j) = ATAN2(sqrt(mom(j)), sqrt(1-mom(j)))
-                        mom(j) = 6371000 * (2 * dad(j))
+                do j=i+1,3260,1
+                        mom(j) = (SIN((lat(i)-lat(j))/2.0))**2+COS(lat(i)) * COS(lat(j))*(SIN((long(i)-long(j))/2.0))**2 
+                        dad(j) = ATAN2(sqrt(mom(j)), sqrt(1.0-mom(j)))
+                        mom(j) = 6373000.0 * (2.0 * dad(j))
                         
                         print *, mom(j)
                 enddo
