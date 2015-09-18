ea51d98698c75a3bcb3d637d71e165e9036a9490
Jonathan Oakes <darth62969@gmail.com>
2015-09-18 13:09:59 -0400

    thins

--- this.f90
+++ this.f90
@@ -5,6 +5,10 @@
         DOUBLE PRECISION, DIMENSION (3260) :: merc
         DOUBLE PRECISION, DIMENSION (3260) ::  mom
         DOUBLE PRECISION, DIMENSION (3260) ::  dad
+        DOUBLE PRECISION, DIMENSION (3260) :: merc2
+        real*8 Dlat
+        real*8 Dlong 
+        
 
         INTEGER cnt
 
@@ -13,11 +17,16 @@
 
 10      read (8,*,END=20) lat(cnt), long(cnt), merc (cnt)
         
- !       print *, 'latitude: ', lat (cnt) 
-  !      print *, 'longitude: ', long (cnt) 
+        print *, 'latitude: ', lat (cnt) 
+        print *, 'longitude: ', long (cnt) 
    !     print *, 'mercury: ', merc (cnt)
     !    print *
-       
+        lat(cnt) = lat(cnt)*ATAN(-1.0)/180
+        long(cnt) = long(cnt)*ATAN(-1.0)/180
+        
+        print *, lat(cnt)
+        print *, long(cnt)
+        
         cnt = cnt + 1
 
         goto 10 	
@@ -27,14 +36,27 @@
         
 
         
-        do i=0,3260,1
+        do i=1,3260,1
                 do j=i+1,3260,1
-                        mom(j) = (SIN((lat(i)-lat(j))/2.0))**2+COS(lat(i)) * COS(lat(j))*(SIN((long(i)-long(j))/2.0))**2 
-                        dad(j) = ATAN2(sqrt(mom(j)), sqrt(1.0-mom(j)))
-                        mom(j) = 6373000.0 * (2.0 * dad(j))
-                        
+
+
+                        Dlong = long(i)-long(j)
+                        Dlat = lat(i)-lat(j)
+
+                        mom(j) = SIN(Dlat/2.0)**2 + (COS(lat(i)) * cos(lat(j)) * sin(Dlong/2.0)**2)
+                        dad(j) = ASIN(sqrt(mom(j)))
+                        mom(j) = 2.0 * 6373000.0 * (dad(j))
+
+  !                      if (mom(j) .lt. 1)
+      !                          cnt= cnt+1
+      !                          merc2(i) = merc2(i)+(merc(i)-merc(j)**2)/(2*cnt)
+!                                  
+                                                             
+    !                    endif
+
                         print *, mom(j)
                 enddo
+        !        print *, merc2((i)
         enddo
         end
 
