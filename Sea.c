73656a97169dafd320823973d845f742e1a7db3e
marvelmugen@gmail.com <marvelmugen@gmail.com>
2015-09-21 13:47:11 -0400

    C mostly works, but it's mega slow

--- Sea.c
+++ Sea.c
@@ -3,50 +3,129 @@
 #include <stdlib.h> 
 #include <math.h>
 
+
+
+double radionConvert(double number){
+double result=(number*M_PI)/180.0;
+return result;
+}
+
 int main(){ 
+
+
 
 FILE *mercuryFile;
 mercuryFile=fopen("mercurydata.csv","r");
 double Longitude[9999];
 double Latitude[9999];
 double Zee[9999];
+int Narray[9999];
+double SumArray[9999];
+
 char info[100000]; 
 char *pointer;
 char *end;
-int a=0;
+double a=0;
 int n=0;
-int d=0;
-int r= 6733000;
+double d=0;
+double r= 6373000.0;
+int count=0;
+int c=0;
+double Sum=0; 
+int iclone;
+double BigD=0;
+static int z=0;
+double lagMax=0.5;
+double lagMin=0.0;
+
 	while (fscanf(mercuryFile,"%s", info)!=EOF){
 	pointer=strtok(info,", \n");
-	Longitude[n]= atof(pointer);
+		Latitude[c]=radionConvert(atof(pointer));
 	pointer=strtok(NULL,", \n");  
-  	Latitude[n]=atof(pointer);
+  
+Longitude[c]= radionConvert(atof(pointer));
 	pointer=strtok(NULL,", \n"); 
-	Zee[n]=atof(pointer);
-	printf("%.10f\n",Longitude[n]); 
-	printf("%.10f\n",Latitude[n]); 
-	printf("%.10f\n",Zee[n]); 
-	n=n+1;
+	Zee[c]=atof(pointer);
+        c=c+1;
+
 	}
-int c=0;
+
+
 //LEZ DO SOME MATH!
-double Sum=0;
+
+
+
+do{
+n=0;
+Sum=0;
 int i=1;
 int j=i+1;
+iclone=i;
+
 	do{
-	i=i+1;
-	j=i+1;
+	
+	j=iclone+1;
+	
+
+
 		do{
-		c=c+1;
-		//if distance is less than a thing
-		Sum=Sum+pow((Zee[i]-Zee[j]),2)/(2*n+0.0);
-		j=j+1;
-		d=r*(2 * atan2(sqrt(a),sqrt(1-a)));
 		
-		}while(j<n);
-	}while(i<n);
+		
+		double dlat=Latitude[iclone]-Latitude[j];
+		double dlong=Longitude[iclone]-Longitude[j];
+		a= pow(sin(dlat/2.0),2) + cos(Latitude[iclone]) * cos(Latitude[j]) * pow(sin(dlong/2.0),2);
+		
+		d=r*(2 * atan2(sqrt(a),sqrt(1-a)));
+
+	
+				if(j<c)
+				if(BigD<d)
+				BigD=d;
+				
+	
+				
+				
+		if(d<=lagMax && d>=lagMin){
+		n=n+1;
+		}
+		j++;
+		
+		}while(j<c);
+	
+iclone=iclone+1;
+}while(c>iclone);
+
+
+do{
+	
+	j=i+1;
+	
+
+
+		do{
+		
+		
+		double dlat=Latitude[i]-Latitude[j];
+		double dlong=Longitude[i]-Longitude[j];
+		a= pow(sin(dlat/2.0),2) + cos(Latitude[i]) * cos(Latitude[j]) * pow(sin(dlong/2.0),2);
+		
+		d=r*(2 * atan2(sqrt(a),sqrt(1-a)));
+
+				
+		if(d<=lagMax && d>=lagMin){
+		Sum=Sum+pow((Zee[i]-Zee[j]),2)/(2*(n+0.0));
+		}
+		j++;
+		
+		}while(j<c);
+	
+i=i+1;
+}while(c>i);
 printf("%.10f\n",Sum);
 printf("%i\n",n);
-printf("%i\n",c);
-}     
+lagMin=lagMin+0.5;
+lagMax=lagMax+0.5;
+}while(n!=0); //This condition is wrong
+
+}
+
