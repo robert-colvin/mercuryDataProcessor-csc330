a72c3512adbfcf5647d0e5f83229ab99c79a479b
Robert Colvin <rcdc008@yahoo.com>
2015-09-23 19:59:07 -0400

    FINAL GERTRUDE

--- Gertrude.java
+++ Gertrude.java
@@ -13,14 +13,13 @@ public class Gertrude
 		ArrayList<Double> latitudes = new ArrayList<Double>();
 		ArrayList<Double> longitudes = new ArrayList<Double>();
 		ArrayList<Double> concentrations = new ArrayList<Double>();
-		//ArrayList<Pair> pointy = new ArrayList<Pair>();
-		int maxD = 769;
+		int maxD = 0;
 		int pointsInLag = 0;
 		double totalD = 0.0;
 		double mercs = 0.0;
 		
 		try
-		{
+		{//GRABBING/STORING DATA FROM FILE
 		    File reader = new File("mercurydata.csv");;
 		    Scanner in = new Scanner(reader);
 
@@ -33,45 +32,58 @@ public class Gertrude
 		        concentrations.add(Double.parseDouble(zeus.nextToken()));
 		        
 		    } 
-		//MAX DISTANCE LOOP
-		    /*for (int i = 0; i < latitudes.size()-1; i++)
-		    {
-			for (int j = i+1; j < latitudes.size(); j++)
-			{
-				double compared = getDistance(latitudes.get(i), latitudes.get(j), longitudes.get(i), longitudes.get(j));
-				if (compared > testy){
-					testy = compared;
-						xlat = latitudes.get(i);
-						xlong = longitudes.get(i);
-						ylat = latitudes.get(j);
-						ylong = longitudes.get(j);}
-			}
 
-		    }
-			System.out.println(testy);*/
 		}
 		catch (IOException ex) 
 		{
 		    System.out.println("WEB URL NOT FOUND.");
 		}
-			//CRAZY EXPERIMENTAL MATH GARBAGE
+
+		//MAX DISTANCE LOOP
+		for (int i = 0; i < latitudes.size()-1; i++)
+		{
+			for (int j = i+1; j < latitudes.size(); j++)
+			{
+				double compared = getDistance(latitudes.get(i), latitudes.get(j), longitudes.get(i), longitudes.get(j));
+				if (compared > testy)
+					testy = compared;
+			}
+			//SAVE MAX DISTANCE AS NEAREST INT SO THAT LAGS CAN COMPLETELY INCREMENT BY 1
+			maxD=Math.round((float) testy);
+		}
+
+
+		//POPULATING THE DISTANCE MATRIX, HENCE THE NAME LONGNEO
+		double[][] longNeo = new double[latitudes.size()][latitudes.size()];
+		for (int i = 0; i < latitudes.size()-1;i++)
+		{
+			for (int j = i+1; j < latitudes.size(); j++)
+			{
+				longNeo[i][j] = getDistance(latitudes.get(i), latitudes.get(j), longitudes.get(i), longitudes.get(j));
+			}
+		}
+		//System.out.println(longNeo[0][0]);
+		//CALCULATE SEMIVARIANCE WITH SOME WILD EQUATION I CANT QUITE GRASP
 
 		for (int lag = 0; lag <= maxD; lag++)
 		{
 			double semivariance = 0.0;
-			for (int i = 0; i < latitudes.size()-1;i++)
+			
+			for (int i = 0; i<longNeo.length;i++)
 			{
-				for (int j = i+1; j < latitudes.size();j++)
+				for (int j = i+1; j< longNeo.length;j++)
 				{
-					if ((double) lag - 0.5 <= getDistance(latitudes.get(i), latitudes.get(j), longitudes.get(i), longitudes.get(j)) 					&& getDistance(latitudes.get(i), latitudes.get(j), longitudes.get(i), longitudes.get(j)) <= (double) lag + 0.5)
+					if ((double) lag - 0.5 <= longNeo[i][j] 
+					&& longNeo[i][j] <= (double) lag + 0.5)
 					{
 						mercs = Math.pow(concentrations.get(i)-concentrations.get(j), 2);
 						semivariance += mercs;
-						totalD += getDistance(latitudes.get(i), latitudes.get(j), longitudes.get(i), longitudes.get(j));
+						totalD += longNeo[i][j];
 						pointsInLag++;
 					}
 				}
-			}
+			}	
+			
 			System.out.println("The # of points in lag " + lag + " is " + pointsInLag);
 			System.out.println("The average distance between points in lag " + lag + " is " + totalD/pointsInLag);
 			semivariance = semivariance/(2*pointsInLag);
